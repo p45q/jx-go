a6c316c77e59f8cff7604db0d0ca678ed4f23742
pascal01 <p45q89@gmail.com>
2019-07-10 09:35:24 +0200

    fix typo

--- main.go
+++ main.go
@@ -17,7 +17,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		log.Printf("title: %s\n", title)
 	}
 
-	fmt.Fprintf(w, "Hello from:  "+title+"\n Cool contentas sadjkasdk")
+	fmt.Fprintf(w, "Hello from:  "+title+"\n Cool contentas")
 }
 
 func main() {
